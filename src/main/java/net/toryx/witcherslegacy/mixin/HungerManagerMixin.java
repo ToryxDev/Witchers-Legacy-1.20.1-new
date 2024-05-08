de9f6d080adb6ed04b4b4aa2f6959afc4a564701
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-08 16:14:36 +1000

    Cin... please I beg of you... test your mod properly...

--- src/main/java/net/toryx/witcherslegacy/mixin/HungerManagerMixin.java
+++ src/main/java/net/toryx/witcherslegacy/mixin/HungerManagerMixin.java
@@ -4,14 +4,13 @@ import net.minecraft.entity.player.*;
 
 import net.toryx.witcherslegacy.WitchersLegacy;
 
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//TODO: Fix code to use the this.prevFoodLevel() and this.foodLevel() etc, or find a better way to do it.
 /**
  * Oh hey a convenient spot for a javadoc! <br>
- * So, this file should make it so that the player will still lose hunger even if they have the bleeding effect. <br>
+ * Just no. AAAAAA MY BRAINCELLS!!! <br>
  * Ok bye - Mavity
  */
 @Mixin(HungerManager.class)
@@ -19,15 +18,7 @@ public abstract class HungerManagerMixin {
     @Inject(at = @At("HEAD"), method = "update")
     private void override_method(PlayerEntity player, CallbackInfo info) {
         if(player.hasStatusEffect(WitchersLegacy.BLEED)) {
-            /*((HungerManager)(Object)this).prevFoodLevel() = ((HungerManager)(Object)this).foodLevel();
-            if (this.exhaustion > 4.0f) {
-                this.exhaustion -= 4.0f;
-                if (this.saturationLevel > 0.0f) {
-                    this.saturationLevel = Math.max(this.saturationLevel - 1.0f, 0.0f);
-                } else if (difficulty != Difficulty.PEACEFUL) {
-                    this.foodLevel = Math.max(this.foodLevel - 1, 0);
-                }
-            }*/
+            // Not required - YOU DIDN'T ACTUALLY TEST IT PROPERLY CIN!!!
         }
     }
 }
